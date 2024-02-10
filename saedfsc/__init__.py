5cbb8441021b632052798de72029d4a39a4314ed
hmedal <hugh.medal@gmail.com>
2024-02-10 16:07:51 -0500

    Reversed sign of stdDevDemandVar constraint. Changed utility for competitors.

--- saedfsc/__init__.py
+++ saedfsc/__init__.py
@@ -88,6 +88,71 @@ def getMarketShare(car : COTSCar, c : str, competitors : List[COTSCar], utilFn =
     totalCompetitorUtility = sum([getTotalUtilityForCustomer(competitorCar, c, utilFn) for competitorCar in competitors])
     return carUtility / (totalCompetitorUtility + carUtility)
 
+def make_nominal_part_prices(broad_part_names, part_options):
+    nominalPartPrices = {}
+    for broad_part in broad_part_names:
+        broad_part_price_dict = {}
+        for specific_parts in range(len(part_options[broad_part])):
+            if broad_part == 'wings':
+                
+                broad_part_price_dict[part_options['wings'].index[specific_parts]] = round(part_options['wings'].iloc[specific_parts]['length'] * 
+                                                                                     part_options['wings'].iloc[specific_parts]['width'] * 
+                                                                                     part_options['wings'].iloc[specific_parts]['height'] * 
+                                                                                     part_options['wings'].iloc[specific_parts]['q'] * 
+                                                                                     part_options['wings'].iloc[specific_parts]['cost_per_kilogram'], 2)
+            
+            elif broad_part == 'tires':
+                
+                broad_part_price_dict[part_options['tires'].index[specific_parts]] = round(part_options['tires'].iloc[specific_parts]['cost'] * 
+                                                                                     (1 + (part_options['tires'].iloc[specific_parts]['pressure'] - 0.758) / 2), 2)
+                                            
+                
+            elif broad_part == 'engine':
+                
+                broad_part_price_dict[part_options['engine'].index[specific_parts]] = round(part_options['engine'].iloc[specific_parts]['Cost'], 2)
+                
+            elif broad_part == 'cabin':
+                
+                broad_part_price_dict[part_options['cabin'].index[specific_parts]] = round(2 * 
+                                                                                     part_options['cabin'].iloc[specific_parts]['thickness'] * 
+                                                                                     (part_options['cabin'].iloc[specific_parts]['length'] * 
+                                                                                      part_options['cabin'].iloc[specific_parts]['width'] + 
+                                                                                      part_options['cabin'].iloc[specific_parts]['length'] * 
+                                                                                      part_options['cabin'].iloc[specific_parts]['height'] + 
+                                                                                      part_options['cabin'].iloc[specific_parts]['width'] * 
+                                                                                      part_options['cabin'].iloc[specific_parts]['height']  
+                                                                                     ) *
+                                                                                     part_options['cabin'].iloc[specific_parts]['q'] * 
+                                                                                     part_options['cabin'].iloc[specific_parts]['cost_per_kilogram'], 2)
+                
+            elif broad_part == 'brakes':
+                
+                broad_part_price_dict[part_options['brakes'].index[specific_parts]] = round(part_options['brakes'].iloc[specific_parts]['lbrk'] * 
+                                                                                      part_options['brakes'].iloc[specific_parts]['wbrk'] * 
+                                                                                      part_options['brakes'].iloc[specific_parts]['hbrk'] * 
+                                                                                      part_options['brakes'].iloc[specific_parts]['qbrk'] * 
+                                                                                      1000 * 
+                                                                                      25, 2)
+                                            
+                
+            elif broad_part == 'impactattenuator':
+                
+                broad_part_price_dict[part_options['impactattenuator'].index[specific_parts]] = round(part_options['impactattenuator'].iloc[specific_parts]['length'] * 
+                                                                                                part_options['impactattenuator'].iloc[specific_parts]['width'] * 
+                                                                                                part_options['impactattenuator'].iloc[specific_parts]['height'] * 
+                                                                                                part_options['impactattenuator'].iloc[specific_parts]['q'] * 
+                                                                                                part_options['impactattenuator'].iloc[specific_parts]['cost_per_kilogram'], 2)
+                
+                
+            elif broad_part == 'suspension':
+                
+                broad_part_price_dict[part_options['suspension'].index[specific_parts]] = 0 # Assuming suspension has fixed cost and is "tuned" (Comment from Dr. McComb's code)
+            
+            
+        nominalPartPrices[broad_part] = broad_part_price_dict
+        
+    return nominalPartPrices
+
 def handleStatus(m : gp.Model):
     status = m.status
     if status == GRB.Status.INFEASIBLE:
